9adb0a244e3aae45824d28894d26cca488ae9d93
t1geryan <sgta1980@gmail.com>
2024-05-15 22:02:32 +0300

    Implemented AutoInjectable and Injector

---
+++ src/main/java/org/example/di/Injector.java
@@ -0,0 +1,36 @@
+package org.example.di;
+
+import java.lang.reflect.Field;
+import java.util.Properties;
+
+public class Injector {
+    private final Properties config;
+
+    public Injector(Properties config) {
+        this.config = config;
+    }
+
+    public <T> void inject(T instance) {
+        Class<?> clazz = instance.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+
+        for (final Field field : fields) {
+            if (field.isAnnotationPresent(AutoInjectable.class)) {
+                Class<?> fieldType = field.getType();
+                String dependencyInstanceClassName = config.getProperty(fieldType.getName());
+                if (dependencyInstanceClassName != null) {
+                    try {
+                        Class<?> dependencyInstanceClass = Class.forName(dependencyInstanceClassName);
+                        Object dependencyInstance = dependencyInstanceClass.getDeclaredConstructor().newInstance();
+                        field.setAccessible(true);
+                        field.set(instance, dependencyInstance);
+                    } catch (Exception e) {
+                        throw new RuntimeException("There is error while providing dependency for " + instance.getClass().getName(), e);
+                    }
+                } else {
+                    throw new RuntimeException("There is no declared implementation of " + fieldType.getName());
+                }
+            }
+        }
+    }
+}
